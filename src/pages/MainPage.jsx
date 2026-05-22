0226a41ba7c664a1da3b3371c015ef4f01eda9ee
pmont4 <pauloamont2005@outlook.com>
2026-05-22 12:47:25 -0600

    .

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,7 +6,6 @@ export const MainPage = () => {
         <div
             className="flex-fill d-flex flex-column w-100"
             style={{
-                // minHeight: '85vh' eliminado por redundancia e inestabilidad
                 backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)), url(${watermarkLogo})`,
                 backgroundSize: 'auto 115%',
                 backgroundPosition: 'calc(100% + 55px) center',
