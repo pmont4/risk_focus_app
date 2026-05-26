fd97c65f8456137c801c2b78487f07a509b50309
pmont4 <pauloamont2005@outlook.com>
2026-05-26 12:32:42 -0600

    Adding line for separation with gradient.

--- src/components/views/ReportListView.jsx
+++ src/components/views/ReportListView.jsx
@@ -17,6 +17,13 @@ export const ReportListView = () => {
     return (
         <div className="d-flex flex-column gap-3">
             <h2>Lista de Reportes</h2>
+            <div
+                style={{
+                    height: '2px',
+                    background: 'linear-gradient(to right, #000000ff, #000000ff 30%, transparent 100%)',
+                    borderRadius: '1px',
+                }}
+            />
             {reportList.length === 0 ? (
                 <p>No hay reportes disponibles.</p>
             ) : (
