007a44a09bc47a0163ef411146ba7dc146884f66
pmont4 <pauloamont2005@outlook.com>
2026-05-29 11:22:56 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import { RiskFocusApp } from './app/riskFocusApp'
+import { RiskFocusApp } from './app/RiskFocusApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
